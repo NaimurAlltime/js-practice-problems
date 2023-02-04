0a666962a9f493126cf273fe0c24243328e3a059
Naimur Rahman <naimur2231@gmail.com>
2023-02-04 11:04:17 +0600

    explore lower and upper case

---
+++ case.js
@@ -0,0 +1,13 @@
+const userName = 'Sabbir Hossain';
+const userInput = 'sabbir Hossain';
+
+if(userName.toLowerCase() === userInput.toLowerCase()){
+    console.log("Valid User");
+}
+else{
+   console.log("Invalid User");
+}
+
+let myName = "Akash ahammed";
+myName = myName.toUpperCase();
+console.log(myName);
