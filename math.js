529f18bd78b75ad139c2e020ba91be1ca9244c71
Naimur Rahman <naimur2231@gmail.com>
2023-02-04 15:59:36 +0600

    math function in js

---
+++ math.js
@@ -0,0 +1,20 @@
+// console.log(Math.pow(5, 3));
+
+const num1 = 20;
+const num2 = 30;
+
+gap = Math.abs(num1 - num2);
+
+if(gap >= 10){
+    console.log("Exit");
+}
+else{
+    console.log("Doesn't Exit");
+}
+
+const result = 4.24542345;
+console.log(Math.round(result));
+console.log(Math.ceil(result));
+console.log(Math.floor(result));
+
+console.log(Math.random());
