2a7fdbaa083c26f7e8193bea2cea62bd4736da7e
Naimur Rahman <naimur2231@gmail.com>
2023-02-04 14:46:54 +0600

    explore indexOf startsWith endsWith

---
+++ indexOf.js
@@ -0,0 +1,14 @@
+const nationalAnthem = 'amar sonar bangla ami tomay valobasi';
+console.log(nationalAnthem.indexOf('bangla'));
+console.log(nationalAnthem.indexOf('ami'));
+console.log('--------------');
+
+// startsWith 
+console.log(nationalAnthem.startsWith('amar'));
+console.log('--------------');
+
+//endsWith 
+// problem: find the .js file 
+const fileName = 'index.js';
+console.log(fileName.endsWith('.js')); 
+
